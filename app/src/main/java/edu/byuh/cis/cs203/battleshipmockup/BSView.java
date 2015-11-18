c47d0afc93471813d014ac193157030e36deea0a
Steven Rodriguez <Steven Rodriguez>
2015-11-18 09:46:07 -1000

    now displays battleship

--- app/src/main/java/edu/byuh/cis/cs203/battleshipmockup/BSView.java
+++ app/src/main/java/edu/byuh/cis/cs203/battleshipmockup/BSView.java
@@ -3,17 +3,36 @@ package edu.byuh.cis.cs203.battleshipmockup;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.view.View;
 
 public class BSView extends View{
-    Bitmap battleship;
+
+    private Bitmap battleship;
+    private Bitmap big_airplane;
+    private Bitmap big_submarine;
+    private Bitmap little_airplane;
+    private Bitmap little_submarine;
+    private Bitmap medium_airplane;
+    private Bitmap medium_submarine;
+    private Bitmap water;
+
+
     public BSView(Context context) {
         super(context);
 
-        Bitmap battleship = BitmapFactory.decodeResource(getResources(), R.drawable.battleship);
+        battleship = BitmapFactory.decodeResource(getResources(), R.drawable.battleship);
+        big_airplane = BitmapFactory.decodeResource(getResources(), R.drawable.big_airplane);
+
     }
 
-    public void onDraw(){
+    public void onDraw(Canvas c){
+        int h = getHeight();
+        int w = getWidth();
 
+        Paint forbitmap = new Paint();
+        battleship = battleship.createScaledBitmap(battleship, (int)(w*.4), (int)(h*.2), true);
+        c.drawBitmap(battleship, w * .5f, h * .3f, forbitmap);
     }
 }
